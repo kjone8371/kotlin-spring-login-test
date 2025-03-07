558a98aae72f7e82491e027c45abbfc3c6ed0ea9
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-03-07 21:17:08 +0900

    IdFilter

---
+++ src/main/kotlin/com/kjone/springkotlinjwt/global/common/id/IdWrapper.kt
@@ -0,0 +1,9 @@
+package com.kjone.springkotlinjwt.global.common.id
+
+import org.springframework.data.jpa.repository.JpaRepository
+import kotlin.jvm.optionals.getOrNull
+
+sealed interface IdWrapper<ID : Any> {
+    val id: ID
+    fun <T : Any> fetch(repository: JpaRepository<T, ID>) = repository.findById(id).getOrNull()
+}
