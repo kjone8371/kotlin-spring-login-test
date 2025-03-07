a7dd99b038fcedd5c60ad50e42efa354d6ed151c
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-03-07 21:08:06 +0900

    SpringKotlinApplication start

---
+++ src/main/kotlin/com/kjone/springkotlinjwt/SpringKotlinJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.kjone.springkotlinjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinJwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinJwtApplication>(*args)
+}
