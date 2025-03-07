0e30e7b9acf900f977e996765d50cf89c9a69377
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-03-07 21:07:22 +0900

    feat::config

---
+++ src/main/kotlin/com/kjone/springkotlinjwt/global/security/auditing/AuditingConfig.kt
@@ -0,0 +1,8 @@
+package com.kjone.springkotlinjwt.global.security.auditing
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class AuditingConfig
